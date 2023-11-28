48902df78bead48bcfa439ae69fb102fe4a4bb59
Heather Floyd <heatherflo@gmail.com>
2023-11-27 21:16:30 -0700

    buttons work

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-function buttonclicked(){
-  console.log("Button Clicked")
+let home = 0
+let away = 0
+
+
+function resetClicked(){
+  console.log("Reset Clicked")
+}
+
+function clickHome1(){
+  console.log("fool")
+  home += 1
+  console.log(home)
+
+  updateHome()
+}
+
+function clickHome3(){
+  console.log("fool")
+  home +=3
+  console.log(home)
+}
+
+function clickAway1(){
+  console.log("away")
+  away += 1
+  console.log(away)
+}
+
+function clickAway3(){
+  console.log("away")
+  away += 3
+  console.log(away)
+}
+function updateHome(){
+  document.getElementById("home").innerHTML = home
+  console.log("Home Updated")
 }
