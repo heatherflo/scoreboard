b75a4c834ecc7dbf5eaf5df230c2433c8cec6550
Heather Floyd <heatherflo@gmail.com>
2023-11-27 23:39:38 -0700

    all done but alert not working on chrome

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let away = 0
 
 function resetClicked(){
   console.log("Reset Clicked")
+  home = 0
+  away = 0
+  updateAway()
+  updateHome() 
 }
 
 function clickHome1(){
@@ -12,26 +16,53 @@ function clickHome1(){
   console.log(home)
 
   updateHome()
+  alert()
 }
 
 function clickHome3(){
-  console.log("fool")
+  console.log("gross")
   home +=3
   console.log(home)
+  updateHome()
+  alert()
 }
 
 function clickAway1(){
-  console.log("away")
+  console.log("no away")
   away += 1
   console.log(away)
+  updateAway()
+  alert()
 }
 
 function clickAway3(){
-  console.log("away")
+  //update away score +3
+  //add score to the board
+  console.log("yay")
   away += 3
   console.log(away)
+  updateAway()
+  alert()
 }
+
 function updateHome(){
-  document.getElementById("home").innerHTML = home
-  console.log("Home Updated")
+  
+  document.getElementById("home").innerText = home
+  //add the new scores to the board 
+  console.log("Score Updated")
 }
+
+function updateAway(){
+  document.getElementById("away").innerText = away
+  console.log("Score Updated")
+}
+
+function alert(){
+  if(home >= 10 ){
+    console.log("home wins")
+    window.alert(home wins!)
+  } else (away >= 10); {
+    window.alert("away wins!")
+}
+} 
+
